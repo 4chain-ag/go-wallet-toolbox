909f775de750da6db1acd29b13e82ed353e7687c
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-03-26 15:20:38 +0100

    feat: validate incoming args

--- pkg/storage/internal/actions/create.go
+++ pkg/storage/internal/actions/create.go
@@ -41,9 +41,53 @@ func newCreateAction(logger *slog.Logger, funder Funder) *create {
 }
 
 func (c *create) Create(auth wdk.AuthID, args wdk.ValidCreateActionArgs) (*wdk.StorageCreateActionResult, error) {
+
 	result, err := c.funder.Fund(context.Background(), nil, *auth.UserID)
 	if err != nil {
 		return nil, fmt.Errorf("funding failed: %w", err)
 	}
 	panic(fmt.Errorf("not implemented %v", result))
+}
+
+func (c *create) validateArgs(args *wdk.ValidCreateActionArgs) error {
+	deducedIsSendWith := len(args.Options.SendWith) > 0
+	if args.IsSendWith != deducedIsSendWith {
+		return fmt.Errorf("inconsistent IsSendWith with Options.SendWith")
+	}
+
+	deducedIsRemixChange := !args.IsSendWith && len(args.Inputs) == 0 && len(args.Outputs) == 0
+	if args.IsRemixChange != deducedIsRemixChange {
+		return fmt.Errorf("inconsistent IsRemixChange with IsSendWith and Inputs and Outputs")
+	}
+
+	deducedIsNewTx := args.IsRemixChange || len(args.Inputs) > 0 || len(args.Outputs) > 0
+	if args.IsNewTx != deducedIsNewTx {
+		return fmt.Errorf("inconsistent IsNewTx with IsRemixChange and Inputs and Outputs")
+	}
+
+	deducedIsSignAction := args.IsNewTx && !args.Options.SignAndProcess && containsNilUnlockingScript(args.Inputs)
+	if args.IsSignAction != deducedIsSignAction {
+		return fmt.Errorf("inconsistent IsSignAction with IsNewTx and Options.SignAndProcess and Inputs.UnlockingScript")
+	}
+
+	deducedIsDelayed := bool(args.Options.AcceptDelayedBroadcast)
+	if args.IsDelayed != deducedIsDelayed {
+		return fmt.Errorf("inconsistent IsDelayed with Options.AcceptDelayedBroadcast")
+	}
+
+	deducedIsNoSend := bool(args.Options.NoSend)
+	if args.IsNoSend != deducedIsNoSend {
+		return fmt.Errorf("inconsistent IsNoSend with Options.NoSend")
+	}
+
+	return nil
+}
+
+func containsNilUnlockingScript(inputs []wdk.ValidCreateActionInput) bool {
+	for _, input := range inputs {
+		if input.UnlockingScript == nil {
+			return true
+		}
+	}
+	return false
 }
