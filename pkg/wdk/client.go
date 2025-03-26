10f062ca366e8b88812c4e32ee4f54757183fa2b
Damian <dzoltow@gmail.com>
2025-03-26 15:09:04 +0100

    lint: fix linter

--- pkg/wdk/client.go
+++ pkg/wdk/client.go
@@ -2,10 +2,12 @@ package wdk
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/filecoin-project/go-jsonrpc"
 )
 
+// NewClient returns WalletStorageWriterClient and allows connection to rpc server
 func NewClient(addr string, overrideOptions ...StorageClientOverrides) (*WalletStorageWriterClient, func(), error) {
 	opts := defaultClientOptions()
 	client := &WalletStorageWriterClient{
@@ -24,6 +26,9 @@ func NewClient(addr string, overrideOptions ...StorageClientOverrides) (*WalletS
 		nil,
 		opts.options...,
 	)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to initialize new RPC client: %w", err)
+	}
 
-	return client, cleanup, err
+	return client, cleanup, nil
 }
