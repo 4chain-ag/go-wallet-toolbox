193a8aa5037bf418fa3383accdc2171acdb6ae9e
chris-4chain <152964795+chris-4chain@users.noreply.github.com>
2025-04-03 11:07:12 +0200

    refactor: repo - CreateUser args not as GORM models (#62)

--- pkg/wdk/table_output_basket.go
+++ pkg/wdk/table_output_basket.go
@@ -4,14 +4,28 @@ import (
 	"time"
 )
 
+// BasketConfiguration is a struct that defines the configuration of the output basket
+type BasketConfiguration struct {
+	Name                    string `json:"name"`
+	NumberOfDesiredUTXOs    int    `json:"numberOfDesiredUTXOs"`
+	MinimumDesiredUTXOValue int    `json:"minimumDesiredUTXOValue"`
+}
+
 // TableOutputBasket is a struct that holds the output baskets details
 type TableOutputBasket struct {
-	CreatedAt               time.Time `json:"created_at"`
-	UpdatedAt               time.Time `json:"updated_at"`
-	BasketID                int       `json:"basketId"`
-	UserID                  int       `json:"userId"`
-	Name                    string    `json:"name"`
-	NumberOfDesiredUTXOs    int       `json:"numberOfDesiredUTXOs"`
-	MinimumDesiredUTXOValue int       `json:"minimumDesiredUTXOValue"`
-	IsDeleted               bool      `json:"isDeleted"`
+	BasketConfiguration `json:",inline"`
+	CreatedAt           time.Time `json:"created_at"`
+	UpdatedAt           time.Time `json:"updated_at"`
+	BasketID            int       `json:"basketId"`
+	UserID              int       `json:"userId"`
+	IsDeleted           bool      `json:"isDeleted"`
+}
+
+// DefaultBasketConfiguration returns a default basket configuration
+func DefaultBasketConfiguration() BasketConfiguration {
+	return BasketConfiguration{
+		Name:                    BasketNameForChange,
+		NumberOfDesiredUTXOs:    32,
+		MinimumDesiredUTXOValue: 1000,
+	}
 }
