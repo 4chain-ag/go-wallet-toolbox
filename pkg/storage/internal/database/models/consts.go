416198e2a844193b7dcc91dd23474ed8490848ac
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-04-04 14:45:50 +0200

    refactor: funder - replace static numbers with txutils

--- pkg/storage/internal/database/models/consts.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// EstimatedInputSizeForP2PKH is the estimated size increase when adding and unlocking P2PKH input to transaction.
-// 32 bytes txID
-// + 4 bytes vout index
-// + 1 byte script length
-// + 107 bytes script pub key
-// + 4 bytes nSequence
-const EstimatedInputSizeForP2PKH = 148
