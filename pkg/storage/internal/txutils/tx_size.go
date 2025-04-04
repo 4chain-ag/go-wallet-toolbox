99a5e264a9a3e410bdb6088ca99f3a3fc2b324da
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-04-04 11:01:43 +0200

    feat: address comments

--- pkg/storage/internal/txutils/tx_size.go
+++ pkg/storage/internal/txutils/tx_size.go
@@ -3,38 +3,30 @@ package txutils
 import (
 	"fmt"
 	"iter"
+
+	sdk "github.com/bsv-blockchain/go-sdk/transaction"
 )
 
-// VarUintSize returns the byte size required to encode number as Bitcoin VarUint
-func VarUintSize(val uint64) uint64 {
-	switch {
-	case val <= 0xfc:
-		return 1
-	case val <= 0xffff:
-		return 3
-	case val <= 0xffffffff:
-		return 5
-	default:
-		return 9
-	}
-}
+const (
+	txEnvelopeSize = 4 + 4 // version + locktime
+	satoshisSize   = 8
+	inputConstSize = 32 + 4 + 4 // txID + vout + sequence
+)
 
 // TransactionInputSize calculates the size in bytes of a transaction input
 // with the given script size
 func TransactionInputSize(scriptSize uint64) uint64 {
-	return 32 + // txid
-		4 + // vout
-		VarUintSize(scriptSize) + // script size in bytes
-		scriptSize + // script
-		4 // sequence number
+	return inputConstSize +
+		varIntSize(scriptSize) +
+		scriptSize
 }
 
 // TransactionOutputSize calculates the serialized byte length of a transaction output
 // with the given script size in bytes
 func TransactionOutputSize(scriptSize uint64) uint64 {
-	return VarUintSize(scriptSize) + // output script length
-		scriptSize + // output script
-		8
+	return varIntSize(scriptSize) +
+		scriptSize +
+		satoshisSize
 }
 
 // TransactionSize calculates the total size of a transaction in bytes
@@ -61,11 +53,20 @@ func TransactionSize(inputSizes iter.Seq2[uint64, error], outputSizes iter.Seq2[
 		outputsSize += TransactionOutputSize(scriptSize)
 	}
 
-	return 4 + // Version
-			VarUintSize(inputsCount) + // Number of inputs
+	return txEnvelopeSize +
+			varIntSize(inputsCount) + // Sizeof value of number of inputs
 			inputsSize + // All inputs accumulated size
-			VarUintSize(outputsCount) + // Number of outputs
-			outputsSize + // All outputs accumulated size
-			4, // lock time
+			varIntSize(outputsCount) + // Sizeof value of number of outputs
+			outputsSize, // All outputs accumulated size
 		nil
+}
+
+func varIntSize(val uint64) uint64 {
+	length := sdk.VarInt(val).Length()
+	return toU64(length)
+}
+
+//nolint:gosec // No need to check for overflows from int to uint64 here
+func toU64(val int) uint64 {
+	return uint64(val)
 }
---
+++ pkg/storage/internal/txutils/tx_size_test.go
@@ -0,0 +1,123 @@
+package txutils
+
+import (
+	"fmt"
+	"iter"
+	"testing"
+
+	"github.com/go-softwarelab/common/pkg/seq"
+	"github.com/go-softwarelab/common/pkg/seqerr"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInputSize(t *testing.T) {
+	// given:
+	unlockingScriptSize := uint64(100)
+
+	// when:
+	size := TransactionInputSize(unlockingScriptSize)
+
+	// then:
+	require.Equal(t, size, 40+unlockingScriptSize+1)
+}
+
+func TestOutputSize(t *testing.T) {
+	// given:
+	lockingScriptSize := uint64(100)
+
+	// when:
+	size := TransactionOutputSize(lockingScriptSize)
+
+	// then:
+	require.Equal(t, size, 8+lockingScriptSize+1)
+}
+
+func TestTransactionSize(t *testing.T) {
+	tests := map[string]struct {
+		inputSizes  iter.Seq[uint64]
+		outputSizes iter.Seq[uint64]
+		expected    uint64
+	}{
+		"two inputs, two outputs": {
+			inputSizes:  seq.Of[uint64](100, 200),
+			outputSizes: seq.Of[uint64](300, 400),
+			expected: 8 + //tx envelope size
+				1 + // varint size of inputs count
+				141 + // 40+100+1 // input [0] size
+				241 + // 40+200+1 // input [1] size
+				1 + // varint size of outputs count
+				311 + // 8+300+3// output [0] size
+				411, // 8+400+3// output [1] size
+		},
+		"zero inputs, two outputs": {
+			inputSizes:  seq.Of[uint64](),
+			outputSizes: seq.Of[uint64](300, 400),
+			expected: 8 + //tx envelope size
+				1 + // varint size of inputs count
+				1 + // varint size of outputs count
+				311 + // 8+300+3// output [0] size
+				411, // 8+400+3// output [1] size
+		},
+		"two inputs, zero outputs": {
+			inputSizes:  seq.Of[uint64](100, 200),
+			outputSizes: seq.Of[uint64](),
+			expected: 8 + //tx envelope size
+				1 + // varint size of inputs count
+				141 + // 40+100+1 // input [0] size
+				241 + // 40+200+1 // input [1] size
+				1, // varint size of outputs count
+		},
+		"300 inputs, 400 outputs": {
+			inputSizes:  seq.Repeat[uint64](100, 300),
+			outputSizes: seq.Repeat[uint64](200, 400),
+			expected: 8 + //tx envelope size
+				3 + // varint size of inputs count
+				300*141 + // 40+100+1 // inputs size
+				3 + // varint size of outputs count
+				400*209, // 8+300+1// outputs size
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// when:
+			size, err := TransactionSize(seqerr.FromSeq(test.inputSizes), seqerr.FromSeq(test.outputSizes))
+
+			// then:
+			require.NoError(t, err)
+			require.Equal(t, size, test.expected)
+		})
+	}
+}
+
+func TestTransactionSizeWithErrorOnInputs(t *testing.T) {
+	// given:
+	inputSizes := seq.Of[uint64](100, 200)
+	outputsSizes := seq.Of[uint64](300, 400)
+
+	// when:
+	_, err := TransactionSize(withErrAtEnd(inputSizes), seqerr.FromSeq(outputsSizes))
+
+	// then:
+	require.Error(t, err)
+}
+
+func TestTransactionSizeWithErrorOnOutputs(t *testing.T) {
+	// given:
+	inputSizes := seq.Of[uint64](100, 200)
+	outputsSizes := seq.Of[uint64](300, 400)
+
+	// when:
+	_, err := TransactionSize(seqerr.FromSeq(inputSizes), withErrAtEnd(outputsSizes))
+
+	// then:
+	require.Error(t, err)
+}
+
+func withErrAtEnd(s iter.Seq[uint64]) iter.Seq2[uint64, error] {
+	return func(yield func(uint64, error) bool) {
+		for v := range s {
+			yield(v, nil)
+		}
+		yield(0, fmt.Errorf("error"))
+	}
+}
