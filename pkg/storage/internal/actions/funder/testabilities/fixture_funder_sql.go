c651ab52b86e965a10aa39c7e1361cfb38041565
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-04-09 08:04:54 +0200

    test: add fixture for basket.

--- pkg/storage/internal/actions/funder/testabilities/fixture_funder_sql.go
+++ pkg/storage/internal/actions/funder/testabilities/fixture_funder_sql.go
@@ -2,6 +2,7 @@ package testabilities
 
 import (
 	"testing"
+	"time"
 
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/defs"
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/internal/logging"
@@ -9,12 +10,16 @@ import (
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/storage/internal/database"
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/storage/internal/database/models"
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/storage/internal/testabilities/dbfixtures"
+	"github.com/4chain-ag/go-wallet-toolbox/pkg/storage/internal/testabilities/testusers"
 	"github.com/stretchr/testify/require"
 )
+
+var exampleDate = time.Date(2006, 02, 01, 15, 4, 5, 7, time.UTC)
 
 type FunderFixture interface {
 	NewFunderService() *funder.SQL
 	UTXO() UserUTXOFixture
+	BasketFor(user testusers.User) BasketFixture
 }
 
 var feeModel = defs.FeeModel{
@@ -51,4 +56,8 @@ func (f *funderFixture) Save(utxo *models.UserUTXO) {
 	err := f.db.DB.Create(&utxo).Error
 	require.NoError(f.t, err)
 	f.createdUTXOs = append(f.createdUTXOs, utxo)
+}
+
+func (f *funderFixture) BasketFor(user testusers.User) BasketFixture {
+	return newBasketFixture(f.t, user)
 }
