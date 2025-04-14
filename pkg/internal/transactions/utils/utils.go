14ca9b02c304517443f707161f671df51e4e2c80
Damian <dzoltow@gmail.com>
2025-04-14 16:05:55 +0200

    feat:lint

--- pkg/internal/transactions/utils/utils.go
+++ pkg/internal/transactions/utils/utils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/internal/hashx"
 )
 
+// TransactionIDFromRawTx will return a transactionID from the rawTx
 func TransactionIDFromRawTx(rawTx []byte) string {
 	hash := hashx.DoubleSha256BE(rawTx)
 	transactionID := hex.EncodeToString(hash)
