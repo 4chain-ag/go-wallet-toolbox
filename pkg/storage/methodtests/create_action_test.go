19e84056b51997a2f22b97b362322e1f71443c07
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-04-16 07:27:02 +0200

    feat: more tests for createAction's resulting outputs

--- pkg/storage/methodtests/create_action_test.go
+++ pkg/storage/methodtests/create_action_test.go
@@ -2,6 +2,8 @@ package methodtests
 
 import (
 	"context"
+	"github.com/4chain-ag/go-wallet-toolbox/pkg/wdk/primitives"
+	"github.com/stretchr/testify/assert"
 	"slices"
 	"testing"
 
@@ -10,7 +12,6 @@ import (
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/storage/internal/testabilities/testusers"
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/storage/testabilities"
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/wdk"
-	"github.com/4chain-ag/go-wallet-toolbox/pkg/wdk/primitives"
 	"github.com/go-softwarelab/common/pkg/to"
 	"github.com/stretchr/testify/require"
 )
@@ -36,6 +37,7 @@ func TestCreateActionHappyPath(t *testing.T) {
 
 	// and:
 	args := fixtures.DefaultValidCreateActionArgs()
+	providedOutput := args.Outputs[0]
 
 	// when:
 	result, err := activeStorage.CreateAction(
@@ -44,16 +46,24 @@ func TestCreateActionHappyPath(t *testing.T) {
 		args,
 	)
 
-	// TODO: Test DB state: but after we make actual getter methods, like ListActions
-
 	// then:
 	require.NoError(t, err)
-	require.Equal(t, 24, len(result.DerivationPrefix))
-	require.Equal(t, 16, len(result.Reference))
-	require.Equal(t, args.Version, result.Version)
-	require.Equal(t, args.LockTime, result.LockTime)
-	require.Equal(t, 1, len(result.Outputs))
-	require.Equal(t, args.Outputs[0].LockingScript, result.Outputs[0].LockingScript)
+	assert.Equal(t, 24, len(result.DerivationPrefix))
+	assert.Equal(t, 16, len(result.Reference))
+	assert.Equal(t, args.Version, result.Version)
+	assert.Equal(t, args.LockTime, result.LockTime)
+	assert.Equal(t, 1, len(result.Outputs))
+
+	resultOutput := result.Outputs[0]
+	assert.Equal(t, wdk.ProvidedByYou, resultOutput.ProvidedBy)
+	assert.Empty(t, resultOutput.Purpose)
+	assert.Equal(t, providedOutput.Satoshis, resultOutput.Satoshis)
+	assert.Equal(t, providedOutput.Basket, resultOutput.Basket)
+	assert.Equal(t, providedOutput.LockingScript, resultOutput.LockingScript)
+	assert.Equal(t, providedOutput.CustomInstructions, resultOutput.CustomInstructions)
+	assert.Equal(t, providedOutput.Tags, resultOutput.Tags)
+
+	// TODO: Test DB state: but after we make actual getter methods, like ListActions
 }
 
 func TestCreateActionWithCommission(t *testing.T) {
@@ -69,22 +79,73 @@ func TestCreateActionWithCommission(t *testing.T) {
 
 	// and:
 	args := fixtures.DefaultValidCreateActionArgs()
-	args.Outputs[0].Basket = to.Ptr(primitives.StringUnder300("custom_basket"))
 
 	// when:
 	result, err := activeStorage.CreateAction(context.Background(), wdk.AuthID{UserID: to.Ptr(testusers.Alice.ID)}, args)
 
 	// then:
 	require.NoError(t, err)
-	require.Equal(t, 24, len(result.DerivationPrefix))
-	require.Equal(t, 16, len(result.Reference))
-	require.Equal(t, args.Version, result.Version)
-	require.Equal(t, args.LockTime, result.LockTime)
-	require.Equal(t, 2, len(result.Outputs))
-	require.True(t, slices.ContainsFunc(result.Outputs, func(p wdk.StorageCreateTransactionSdkOutput) bool {
+	assert.Equal(t, 24, len(result.DerivationPrefix))
+	assert.Equal(t, 16, len(result.Reference))
+	assert.Equal(t, args.Version, result.Version)
+	assert.Equal(t, args.LockTime, result.LockTime)
+	assert.Equal(t, 2, len(result.Outputs))
+
+	commissionOutput, _ := findOutput(t, result.Outputs, func(p wdk.StorageCreateTransactionSdkOutput) bool {
 		return p.Purpose == "storage-commission"
-	}))
-	require.True(t, slices.ContainsFunc(result.Outputs, func(p wdk.StorageCreateTransactionSdkOutput) bool {
-		return p.Purpose == ""
-	}))
+	})
+	assert.Equal(t, primitives.SatoshiValue(10), commissionOutput.Satoshis)
+	assert.Nil(t, commissionOutput.Basket)
+	assert.Equal(t, wdk.ProvidedByStorage, commissionOutput.ProvidedBy)
+	assert.Nil(t, commissionOutput.DerivationSuffix)
+	assert.NotEmpty(t, commissionOutput.LockingScript)
+	assert.NoError(t, commissionOutput.LockingScript.Validate())
+	assert.Empty(t, commissionOutput.OutputDescription)
+	assert.Nil(t, commissionOutput.CustomInstructions)
+	assert.Empty(t, commissionOutput.Tags)
+}
+
+func TestCreateActionShuffleOutputs(t *testing.T) {
+	given := testabilities.Given(t)
+
+	// given:
+	activeStorage := given.Provider().
+		WithCommission(defs.Commission{
+			PubKeyHex: "03398d26f180996f8a2cb175a99620630d76257ccfef4ac7d303c8aa6f90c3190c",
+			Satoshis:  10,
+		}).
+		GORM()
+
+	// and:
+	args := fixtures.DefaultValidCreateActionArgs()
+
+	// when:
+	commissionOutputVouts := map[uint32]struct{}{}
+	for range 100 {
+		result, _ := activeStorage.CreateAction(
+			context.Background(),
+			wdk.AuthID{UserID: to.Ptr(testusers.Bob.ID)},
+			args,
+		)
+
+		found := slices.IndexFunc(result.Outputs, func(p wdk.StorageCreateTransactionSdkOutput) bool {
+			return p.Purpose == "storage-commission"
+		})
+		commissionOutputVouts[result.Outputs[found].Vout] = struct{}{}
+	}
+
+	// then:
+	require.Greater(t, len(commissionOutputVouts), 1)
+}
+
+func findOutput(
+	t *testing.T,
+	outputs []wdk.StorageCreateTransactionSdkOutput,
+	finder func(p wdk.StorageCreateTransactionSdkOutput) bool,
+) (*wdk.StorageCreateTransactionSdkOutput, uint32) {
+	t.Helper()
+	index := slices.IndexFunc(outputs, finder)
+	require.GreaterOrEqual(t, index, 0)
+
+	return &outputs[index], uint32(index)
 }
